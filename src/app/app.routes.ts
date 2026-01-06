6a0a43f7513bf0a3a01f3b922f36576dc881a1df
anouar eil barry <elbarryanwar37@gmail.com>
2026-01-06 19:43:21 +0100

    add routing for login component and redirect to login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/public/login/login.component/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },{
+  path: '',
+  redirectTo: '/login',
+  pathMatch: 'full'
+  }
+];
