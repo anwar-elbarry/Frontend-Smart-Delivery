20aab9d93d9a52bc8ed95df6c91bbbed376c7af6
anouar eil barry <elbarryanwar37@gmail.com>
2026-01-13 15:28:10 +0100

    fix:correct import statement for app.routes module

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import {provideRouter} from '@angular/router';
-import {routes} from './app/app.routes';
+import routes from './app/app.routes';
 
 bootstrapApplication(App, {
   providers: [
