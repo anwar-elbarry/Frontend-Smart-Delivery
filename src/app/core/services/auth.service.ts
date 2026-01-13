891838ada4ec36ba4e3b57227e2b270ea23d179f
anouar eil barry <elbarryanwar37@gmail.com>
2026-01-13 15:27:31 +0100

    feat:enhance auth service with token management and refresh functionality

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,38 +1,90 @@
-import {inject, Injectable, signal} from '@angular/core';
+import {computed, inject, Injectable, signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {UserModel} from '../models/user.model';
 import {tap} from 'rxjs';
+import {CurrentUser} from '../models/auth/current-user.model';
+import {AuthResponse, LoginCredentials} from '../models/auth/auth.model';
+import {UserRole} from '../models/enums/user-role.enum';
+import { environment } from '../../../environment/environment';
+import { Router } from '@angular/router';
+
 @Injectable({
   providedIn: "root"
 })
-export class Auth {
+export class AuthService {
   private http = inject(HttpClient);
-  private apiUrl = "http://localhost:4200/api/auth";
+  private router = inject(Router);
+  private apiUrl = `${environment.apiUrl}/auth`;
 
-  currentUser = signal<UserModel | null>(null);
+  private accessToken = '';
+  private refreshToken = '';
+
+  private currentUser = signal<CurrentUser | null>(null);
+  isAuthenticated = computed(() => !!this.currentUser());
 
   constructor() {
     const savedUser = localStorage.getItem('user');
-    if (savedUser){
+    const savedAccess = localStorage.getItem('accessToken');
+    const savedRefresh = localStorage.getItem('refreshToken');
+
+    if (savedUser && savedAccess && savedRefresh){
       this.currentUser.set(JSON.parse(savedUser));
+      this.accessToken = savedAccess;
+      this.refreshToken = savedRefresh;
     }
   }
 
-  login(credentials: any){
-      return this.http.post<UserModel>(`${this.apiUrl}/login`,credentials).pipe(
-       tap((user) => {
+  login(credentials: LoginCredentials){
+      return this.http.post<AuthResponse>(`${this.apiUrl}/login`,credentials).pipe(
+       tap((resp) => {
+         const user: CurrentUser = {
+           ...resp.user,
+           role: resp.user.roleName as UserRole,
+           enabled: resp.user.enable,
+           isAuthenticated: true
+         };
               this.currentUser.set(user);
-              localStorage.setItem('user',JSON.stringify(user))
+              this.accessToken = resp.accessToken;
+              this.refreshToken = resp.refreshToken;
+
+              // persist everything
+              localStorage.setItem('user',JSON.stringify(user));
+              localStorage.setItem('accessToken', resp.accessToken);
+              localStorage.setItem('refreshToken', resp.refreshToken);
        })
       )
   }
 
-  logout(){
-    this.currentUser.set(null);
-    localStorage.removeItem('user');
+
+  refresh(){
+    return this.http.post<AuthResponse>(`${this.apiUrl}/refresh-token`,{},{
+      headers: {
+        'Authorization': `Bearer ${this.refreshToken}`
+      }
+    }).pipe(
+      tap((resp) => {
+        this.accessToken = resp.accessToken;
+        this.refreshToken = resp.refreshToken;
+
+         localStorage.setItem('accessToken', resp.accessToken);
+         localStorage.setItem('refreshToken', resp.refreshToken);
+      })
+    );
   }
 
-  isAuthenticated(){
-    return  this.currentUser() !== null;
+  getAccessToken(){
+    return this.accessToken;
+  }
+
+  getRefreshToken(){
+    return this.refreshToken;
+  }
+
+  logout(){
+    this.currentUser.set(null);
+    this.accessToken = '';
+    this.refreshToken = '';
+    localStorage.removeItem('user');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('accessToken');
   }
 }
