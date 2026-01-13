a60aa0c5af2f07c5218db2f4b547381c0135a275
anouar eil barry <elbarryanwar37@gmail.com>
2026-01-13 16:41:13 +0100

    feat:integrate sidebar and header components into admin dashboard

--- src/app/features/dashboard/admin/pages/admin-dashboard/admin-dashboard.ts
+++ src/app/features/dashboard/admin/pages/admin-dashboard/admin-dashboard.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {Sidebar} from '../../../../../core/layout/sidebar/sidebar';
+import {RouterOutlet} from '@angular/router';
+import {Header} from '../../../../../core/layout/header/header';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [Sidebar, RouterOutlet, Header],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.css',
 })
